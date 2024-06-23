599bdbcf45c9819265d7599b5fc489831ce9bb7b
Ankit Bansal <ankitbansalniit@outlook.com>
2024-06-23 15:27:30 +0530

    Added Deferrable Views logic.

--- angular-17-features/src/app/user/user.component.ts
+++ angular-17-features/src/app/user/user.component.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
 
-
 @Component({
-    selector: 'app-user',
-    templateUrl: './user.component.html',
-    styleUrl: './user.component.css',
-    standalone: true,
-    imports: [],
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+  standalone: true,
+  imports: [],
 })
 export class UserComponent {
   users: any;
+  showData: boolean = false;
   constructor() {}
   ngOnInit() {
     fetch('http://dummyjson.com/users').then((res) =>
-      res.json().then((data) => (this.users = data.users)).then((users) => {console.log(users)})
+      res
+        .json()
+        .then((data) => {
+          this.users = data.users;
+          setTimeout(() => {
+            this.showData = true;
+          }, 3000);
+
+        })
+        .then((users) => {
+          console.log(users);
+        })
     );
   }
 }
